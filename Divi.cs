886f98c70d71fa55da53a66905424cc8ebd46969
ale1001 <64748782+ale1001@users.noreply.github.com>
2020-05-04 20:46:21 -0300

    Add files via upload

---
+++ Divi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Divi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Essa calculadora calcula a divisão entre 2 números!");
+            int n1, n2, resultado;
+            Console.WriteLine("Informe o primeiro número:");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número;");
+            n2 = int.Parse(Console.ReadLine());
+
+            resultado = n1 / n2;
+
+            Console.WriteLine("O resultado entre a divisão do número " + n1 + " e " + n2 + ", é: " + resultado);
+            Console.ReadKey();
+        }
+    }
+    }
+
